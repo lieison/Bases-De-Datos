ae0e13934402e4eed14c0c5bebb7cd09368878f0
rolignu2 <rolignu90@gmail.com>
2015-04-20 09:10:31 -0600

    .sql

--- AkoyaSv/AKOYA_REPLACE.sql
+++ AkoyaSv/AKOYA_REPLACE.sql
@@ -4,3 +4,11 @@ WHERE post_content LIKE '%http://localhost/AkoyaSv/%';
 
 UPDATE wp_posts SET post_content = 
 replace(post_content , "http://localhost/AkoyaSv/" , "http://lieison.org/akoyasv/dev/");
+
+
+SELECT post_content as 'contenido' 
+FROM wp_akoyasv.wp_posts 
+WHERE post_content LIKE '%http://localhost/AkoyaSv/%';
+
+UPDATE wp_posts SET post_content = 
+replace(post_content , "http://localhost/AkoyaSv/" , "http://akoyasv.com/");
