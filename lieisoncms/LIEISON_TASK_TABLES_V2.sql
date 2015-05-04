22dc6e47a1276a93bc965f6a8f5c6245cd7b0d9f
rolando arriaza <rolignu90@gmail.com>
2015-05-04 15:08:03 -0600

    TABLES

---
+++ lieisoncms/LIEISON_TASK_TABLES_V2.sql
@@ -0,0 +1,172 @@
+CREATE DATABASE  IF NOT EXISTS `lieison_soft` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `lieison_soft`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: localhost    Database: lieison_soft
+-- ------------------------------------------------------
+-- Server version	5.5.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `task_task`
+--
+
+DROP TABLE IF EXISTS `task_task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_task` (
+  `id_task` int(11) NOT NULL AUTO_INCREMENT,
+  `id_multitask` int(11) DEFAULT NULL,
+  `id_type` int(11) NOT NULL,
+  `id_user_from` varchar(255) NOT NULL,
+  `id_user_to` varchar(255) NOT NULL,
+  `id_message` int(11) DEFAULT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `date_asign` date NOT NULL,
+  `time_asign` time NOT NULL,
+  `id_files` int(11) DEFAULT NULL,
+  `comments` text,
+  `date_deadline` date NOT NULL,
+  `time_deadline` time NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_task`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_task`
+--
+
+LOCK TABLES `task_task` WRITE;
+/*!40000 ALTER TABLE `task_task` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task_task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_file`
+--
+
+DROP TABLE IF EXISTS `task_file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_file` (
+  `id_file` int(11) NOT NULL AUTO_INCREMENT,
+  `id_task` int(11) NOT NULL,
+  `name` text,
+  `size` varchar(45) DEFAULT NULL,
+  `url` varchar(45) DEFAULT NULL,
+  `link` varchar(45) DEFAULT NULL,
+  `options` text,
+  PRIMARY KEY (`id_file`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_file`
+--
+
+LOCK TABLES `task_file` WRITE;
+/*!40000 ALTER TABLE `task_file` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task_file` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_type`
+--
+
+DROP TABLE IF EXISTS `task_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_type` (
+  `id_type` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_type`
+--
+
+LOCK TABLES `task_type` WRITE;
+/*!40000 ALTER TABLE `task_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_multitask`
+--
+
+DROP TABLE IF EXISTS `task_multitask`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_multitask` (
+  `id_multitask` int(11) NOT NULL AUTO_INCREMENT,
+  `id_client` int(11) NOT NULL,
+  `id_user_from` varchar(100) DEFAULT NULL,
+  `description` varchar(150) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  `title` text,
+  PRIMARY KEY (`id_multitask`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_multitask`
+--
+
+LOCK TABLES `task_multitask` WRITE;
+/*!40000 ALTER TABLE `task_multitask` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task_multitask` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task_subtask`
+--
+
+DROP TABLE IF EXISTS `task_subtask`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_subtask` (
+  `id_subtask` int(11) NOT NULL AUTO_INCREMENT,
+  `id_task` int(11) NOT NULL,
+  `id_message` int(11) DEFAULT NULL,
+  `id_status` int(11) NOT NULL,
+  `comment` text,
+  `id_user_from` varchar(255) NOT NULL,
+  `id_user_to` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_subtask`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task_subtask`
+--
+
+LOCK TABLES `task_subtask` WRITE;
+/*!40000 ALTER TABLE `task_subtask` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task_subtask` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-26 19:44:36
